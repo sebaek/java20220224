04a6ef03d82dd74b68c5e53f07a98da6aed8d083
sebaek <sebaek@gmail.com>
2022-02-28 11:19:35 +0900

    Update Warmup1.java

--- src/chap99/codingbat/Warmup1.java
+++ src/chap99/codingbat/Warmup1.java
@@ -1,6 +1,12 @@
 package chap99.codingbat;
 
 public class Warmup1 {
+	public int sumDouble(int a, int b) {
+		int sum = a + b;
+
+		return (a == b) ? sum * 2 : sum;
+	}
+
 	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
 		return !(aSmile ^ bSmile);
 	}
