f1c544a710179398fa49d7172079b92fbef566bd
sebaek <sebaek@gmail.com>
2022-02-28 16:03:37 +0900

    alarmClock

--- src/chap99/codingbat/Logic1.java
+++ src/chap99/codingbat/Logic1.java
@@ -1,6 +1,35 @@
 package chap99.codingbat;
 
 public class Logic1 {
+	public String alarmClock(int day, boolean vacation) {
+		String early;
+		String late;
+
+		if (vacation) {
+			early = "10:00";
+			late = "off";
+		} else {
+			early = "7:00";
+			late = "10:00";
+		}
+
+		String result = "";
+		switch (day) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			result = early;
+			break;
+		case 0:
+		case 6:
+			result = late;
+		}
+
+		return result;
+	}
+
 	public int sortaSum(int a, int b) {
 		int sum = a + b;
 
