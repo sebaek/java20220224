13b99e2aed141b1284c03c9500a64fd0cb41492c
sebaek <sebaek@gmail.com>
2022-02-28 11:09:07 +0900

    monkeyTrouble

--- src/chap99/codingbat/Warmup1.java
+++ src/chap99/codingbat/Warmup1.java
@@ -1,6 +1,10 @@
 package chap99.codingbat;
 
 public class Warmup1 {
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		return !(aSmile ^ bSmile);
+	}
+
 	public boolean sleepIn(boolean weekday, boolean vacation) {
 		return !weekday || vacation;
 	}
