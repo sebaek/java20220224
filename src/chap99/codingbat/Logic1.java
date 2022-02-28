2e6b55cc16d839f381cf61a5569dbb359a31b460
sebaek <sebaek@gmail.com>
2022-02-28 12:40:08 +0900

    if else

--- src/chap99/codingbat/Logic1.java
+++ src/chap99/codingbat/Logic1.java
@@ -1,6 +1,21 @@
 package chap99.codingbat;
 
 public class Logic1 {
+	public int dateFashion(int you, int date) {
+		int result = 1;
+
+		if (you >= 8 || date >= 8) {
+			result = 2;
+		}
+
+		if (you <= 2 || date <= 2) {
+			result = 0;
+		}
+
+		return result;
+
+	}
+
 	public boolean cigarParty(int cigars, boolean isWeekend) {
 
 		if (isWeekend) {
