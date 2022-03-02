4bb1e8edc4e5f5efdc18e8d27d96465fd07f0fc8
sebaek <sebaek@gmail.com>
2022-03-02 11:45:49 +0900

    nexted for

--- src/chap04/book/ForMultiplicationTableExample.java
+++ src/chap04/book/ForMultiplicationTableExample.java
@@ -2,6 +2,7 @@ package chap04.book;
 
 public class ForMultiplicationTableExample {
 	public static void main(String[] args) {
+		// 2단 ~ 9단
 		for (int m = 2; m <= 9; m++) {
 			
 			System.out.println("*** " + m + "단 ***");
@@ -10,5 +11,18 @@ public class ForMultiplicationTableExample {
 			}
 			
 		}
+		
+		// 9단 ~ 2단
+		System.out.println("##########9단 ~ 2단");
+
+		// 9 X 9 = 81 ~~~~ 2 X 1 = 2
+		System.out.println("##########9 X 9 = 81 ~~~~ 2 X 1 = 2");
 	}
 }
+
+
+
+
+
+
+
