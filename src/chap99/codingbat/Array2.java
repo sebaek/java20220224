9e8bed74bfeb00ca9ec5f83248d50b9313aa80e0
sebaek <sebaek@gmail.com>
2022-03-02 16:02:05 +0900

    diff min max

--- src/chap99/codingbat/Array2.java
+++ src/chap99/codingbat/Array2.java
@@ -1,6 +1,23 @@
 package chap99.codingbat;
 
 public class Array2 {
+	public int bigDiff(int[] nums) {
+		int max = nums[0];
+		int min = nums[0];
+
+		for (int i = 1; i < nums.length; i++) {
+			if (max < nums[i]) {
+				max = nums[i];
+			}
+
+			if (min > nums[i]) {
+				min = nums[i];
+			}
+		}
+
+		return max - min;
+	}
+
 	public int countEvens(int[] nums) {
 		int cnt = 0;
 
