170a756f8f6cabbc3a891338913f438ace3f0587
sebaek <sebaek@gmail.com>
2022-03-02 12:17:21 +0900

    nested for

--- src/chap04/book/ForMultiplicationTableExample.java
+++ src/chap04/book/ForMultiplicationTableExample.java
@@ -4,21 +4,38 @@ public class ForMultiplicationTableExample {
 	public static void main(String[] args) {
 		// 2단 ~ 9단
 		for (int m = 2; m <= 9; m++) {
-			
+
 			System.out.println("*** " + m + "단 ***");
 			for (int n = 1; n <= 9; n++) {
 				System.out.println(m + " X " + n + " = " + (m * n));
 			}
-			
+
 		}
-		
+
 		// 9단 ~ 2단
 		System.out.println("##########9단 ~ 2단");
+		for (int m = 9; m >= 2; m--) {
+
+			System.out.println("*** " + m + "단 ***");
+			for (int n = 1; n <= 9; n++) {
+				System.out.println(m + " X " + n + " = " + (m * n));
+			}
+
+		}
 
 		// 9 X 9 = 81 ~~~~ 2 X 1 = 2
 		System.out.println("##########9 X 9 = 81 ~~~~ 2 X 1 = 2");
+		for (int m = 9; m >= 2; m--) {
+
+			System.out.println("*** " + m + "단 ***");
+			for (int n = 9; n >= 1; n--) {
+				System.out.println(m + " X " + n + " = " + (m * n));
+			}
+
+		}
 	}
 }
+
 
 
 
---
+++ src/chap04/book/exercise/Exercise061.java
@@ -0,0 +1,42 @@
+package chap04.book.exercise;
+
+public class Exercise061 {
+	public static void main(String[] args) {
+		// *
+		// **
+		// ***
+		// ****
+		// *****
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < (i + 1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		// *****
+		// ****
+		// ***
+		// **
+		// *
+		
+		
+		// *****
+		//  ****
+		//   ***
+		//    **
+		//     *
+		
+		
+		//     *
+		//    **
+		//   ***
+		//  ****
+		// *****
+	}
+}
+
+
+
+
+
