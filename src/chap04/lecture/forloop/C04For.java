b3c5ae1bcb0ca6e3ba21c5fe3ccf9d214b344da8
sebaek <sebaek@gmail.com>
2022-03-02 10:08:54 +0900

    for loop example

--- src/chap04/lecture/forloop/C04For.java
+++ src/chap04/lecture/forloop/C04For.java
@@ -19,7 +19,7 @@ public class C04For {
 		
 		System.out.println("10~0까지 짝수만 출력"); // 10 8 6 4 2 0
 		
-		System.out.println("9~1까지 홀수만 출력"); // 9 7 5 3 2 1
+		System.out.println("9~1까지 홀수만 출력"); // 9 7 5 3 1
 		
 		
 	}
