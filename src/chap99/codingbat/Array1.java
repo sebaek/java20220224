20174e3393923cf73f48d7ef2609de6cb7299ff7
sebaek <sebaek@gmail.com>
2022-03-02 14:41:26 +0900

    array1

--- src/chap99/codingbat/Array1.java
+++ src/chap99/codingbat/Array1.java
@@ -1,6 +1,10 @@
 package chap99.codingbat;
 
 public class Array1 {
+	public boolean commonEnd(int[] a, int[] b) {
+		return (a[0] == b[0]) || (a[a.length - 1] == b[b.length - 1]);
+	}
+
 	public boolean sameFirstLast(int[] nums) {
 
 		if (nums.length > 0) {
