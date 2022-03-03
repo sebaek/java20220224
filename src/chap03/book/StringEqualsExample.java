199849de9d039220ba9c6e869130f6d2e621f0bd
sebaek <sebaek@gmail.com>
2022-03-03 11:27:19 +0900

    scanner, string equals

---
+++ src/chap03/book/StringEqualsExample.java
@@ -0,0 +1,19 @@
+package chap03.book;
+
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		String strVar1 = "신민철";
+		String strVar2 = "신민철";
+		String strVar3 = new String("신민철");
+		
+		System.out.println(strVar1);
+		System.out.println(strVar2);
+		System.out.println(strVar3);
+		
+		System.out.println(strVar1 == strVar2); // true
+		System.out.println(strVar1 == strVar3); // false
+		System.out.println(strVar1.equals(strVar2)); // true
+		System.out.println(strVar1.equals(strVar3)); // true
+		
+	}
+}
