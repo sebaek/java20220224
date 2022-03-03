0967b044a4b851208d8d5a2fa0a440e934533315
sebaek <sebaek@gmail.com>
2022-03-03 10:25:16 +0900

    break

---
+++ src/chap04/lecture/breakcontinue/C01Break.java
@@ -0,0 +1,14 @@
+package chap04.lecture.breakcontinue;
+
+public class C01Break {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			if (i == 5) {
+				break;
+			}
+			System.out.println(i);
+			
+		}
+	}
+}
