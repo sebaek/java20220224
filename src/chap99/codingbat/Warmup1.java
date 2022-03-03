521fd9d80164e7eb587dfc19d4af4e29fa334673
sebaek <sebaek@gmail.com>
2022-03-03 15:34:17 +0900

    posneg

--- src/chap99/codingbat/Warmup1.java
+++ src/chap99/codingbat/Warmup1.java
@@ -1,6 +1,15 @@
 package chap99.codingbat;
 
 public class Warmup1 {
+	public boolean posNeg(int a, int b, boolean negative) {
+
+		if (negative) {
+			return a < 0 && b < 0;
+		} else {
+			return (a * b) < 0;
+		}
+	}
+
 	public boolean nearHundred(int n) {
 		// return (n >= 90 && n <= 110) || (n >= 190 && n <= 210);
 		return Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10;
