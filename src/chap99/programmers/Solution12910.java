038dbc325e969a3500562ddcba4266463bfa001e
sebaek <sebaek@gmail.com>
2022-03-07 15:10:27 +0900

    arrays

--- src/chap99/programmers/Solution12910.java
+++ src/chap99/programmers/Solution12910.java
@@ -2,6 +2,8 @@ package chap99.programmers;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 public class Solution12910 {
@@ -14,7 +16,29 @@ public class Solution12910 {
 	}
 
 	public int[] solution(int[] arr, int divisor) {
-		int[] answer = {};
-		return answer;
+		int[] answer = new int[arr.length];
+
+		int i = 0;
+		int j = 0;
+
+		while (i < arr.length) {
+
+			if ((arr[i] % divisor) == 0) {
+				answer[j] = arr[i];
+				j++;
+			}
+
+			i++;
+		}
+
+		if (j == 0) {
+			return new int[] { -1 };
+		}
+
+		int[] answer2 = Arrays.copyOf(answer, j);
+
+		Arrays.sort(answer2);
+
+		return answer2;
 	}
 }
