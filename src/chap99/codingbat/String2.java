45fb28d19bc0e589c5e04c2e4905ea82ff9426cd
sebaek <sebaek@gmail.com>
2022-03-08 11:43:21 +0900

    catDog

--- src/chap99/codingbat/String2.java
+++ src/chap99/codingbat/String2.java
@@ -1,6 +1,20 @@
 package chap99.codingbat;
 
 public class String2 {
+	public boolean catDog(String str) {
+		int catCnt = 0, dogCnt = 0;
+		for (int i = 0; i < str.length() - 2; i++) {
+			String sub = str.substring(i, i + 3);
+			if (sub.equals("cat")) {
+				catCnt++;
+			} else if (sub.equals("dog")) {
+				dogCnt++;
+			}
+		}
+
+		return catCnt == dogCnt;
+	}
+
 	public int countHi(String str) {
 		int cnt = 0;
 
