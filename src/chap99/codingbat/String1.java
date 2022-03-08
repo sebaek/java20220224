c15452a2cee8aeb419df3af14440552ad7ca310b
sebaek <sebaek@gmail.com>
2022-03-08 10:14:55 +0900

    nTwice

--- src/chap99/codingbat/String1.java
+++ src/chap99/codingbat/String1.java
@@ -1,6 +1,13 @@
 package chap99.codingbat;
 
 public class String1 {
+	public String nTwice(String str, int n) {
+		String front = str.substring(0, n);
+		String end = str.substring(str.length() - n);
+
+		return front + end;
+	}
+
 	public String firstTwo(String str) {
 		if (str.length() < 2) {
 			return str;
