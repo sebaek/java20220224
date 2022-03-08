1eb97a9570be93e49d01a5e1f3477a77c5fb4a5f
sebaek <sebaek@gmail.com>
2022-03-08 15:41:14 +0900

    enhanced for

---
+++ src/chap05/lecture/C02EnhancedFor.java
@@ -0,0 +1,19 @@
+package chap05.lecture;
+
+public class C02EnhancedFor {
+	public static void main(String[] args) {
+		int[] arr = {-1, 2, 10, 13};
+		
+		// 향상된for 사용해서 arr의 모든 값을 더해서 출력
+		
+		int sum = 0;
+		/*
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		*/
+		
+		System.out.println(sum);
+		
+	}
+}
