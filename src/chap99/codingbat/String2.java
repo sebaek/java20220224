805a0eff2cc742ff88bb2ece65d4408a851d76ee
sebaek <sebaek@gmail.com>
2022-03-08 11:27:56 +0900

    countHi

--- src/chap99/codingbat/String2.java
+++ src/chap99/codingbat/String2.java
@@ -1,6 +1,19 @@
 package chap99.codingbat;
 
 public class String2 {
+	public int countHi(String str) {
+		int cnt = 0;
+
+		for (int i = 0; i < str.length() - 1; i++) {
+			String sub = str.substring(i, i + 2);
+			if (sub.equals("hi")) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+
 	public String doubleChar(String str) {
 		String result = "";
 
