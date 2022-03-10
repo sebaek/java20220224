a48424b219ef674f7f5114e1a9f82d121eb1ba58
sebaek <sebaek@gmail.com>
2022-03-10 15:14:24 +0900

    flow

--- src/chap06/lecture/p03constructor/Star.java
+++ src/chap06/lecture/p03constructor/Star.java
@@ -4,12 +4,15 @@ public class Star {
 	String stars;
 	
 	Star(int number) {
+		/*
 		String init = "";
 		for (int i = 0; i < number; i++) {
 			init += "*";
 		}
 		
 		this.stars = init;
+		*/
+		this("*", number);
 	}
 	
 	Star(String shape, int number) {
