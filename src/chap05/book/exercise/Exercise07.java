36f210e8b4863f953ebe531b562f6527cb3a96d8
sebaek <sebaek@gmail.com>
2022-03-10 09:37:46 +0900

    exercise

--- src/chap05/book/exercise/Exercise07.java
+++ src/chap05/book/exercise/Exercise07.java
@@ -7,12 +7,25 @@ public class Exercise07 {
 //		int max = Integer.MIN_VALUE;
 		int max = array[0];
 		
+		/*
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] > max) {
 				max = array[i];
+			}
+		}
+		*/
+		for (int item : array) {
+			if (item > max) {
+				max = item;
 			}
 		}
 		
 		System.out.println("max: " + max);
 	}
 }
+
+
+
+
+
+
