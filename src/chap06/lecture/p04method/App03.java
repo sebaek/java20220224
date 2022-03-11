f317ce33721fa35d59c96d1dd447964e323c872f
sebaek <sebaek@gmail.com>
2022-03-11 10:13:22 +0900

    parameter

--- src/chap06/lecture/p04method/App03.java
+++ src/chap06/lecture/p04method/App03.java
@@ -5,5 +5,13 @@ public class App03 {
 		Computer c1 = new Computer();
 		c1.execute("game");
 		c1.execute("excel");
+		
+		c1.update("calculator");
+		c1.update("windows");
+		
+		c1.add(3, 5);
+		c1.add(10, 20);
+//		c1.add(3.14, 1.0);
+		c1.add('c', 'a');
 	}
 }
