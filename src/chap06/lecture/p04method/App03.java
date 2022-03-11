67f5ee07797b339b0587087959d12b375cd216db
sebaek <sebaek@gmail.com>
2022-03-11 10:08:47 +0900

    parameter

---
+++ src/chap06/lecture/p04method/App03.java
@@ -0,0 +1,9 @@
+package chap06.lecture.p04method;
+
+public class App03 {
+	public static void main(String[] args) {
+		Computer c1 = new Computer();
+		c1.execute("game");
+		c1.execute("excel");
+	}
+}
---
+++ src/chap06/lecture/p04method/Computer.java
@@ -0,0 +1,8 @@
+package chap06.lecture.p04method;
+
+public class Computer {
+	// 메소드명(파라미터목록)
+	void execute(String program) {
+		System.out.println(program);
+	}
+}
