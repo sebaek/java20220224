b7e7fcd4967bd7c1bb696a8f818efa68e269a4d5
sebaek <sebaek@gmail.com>
2022-03-14 10:18:55 +0900

    access modifier on class

--- src/chap06/book/s061301/package1/A.java
+++ src/chap06/book/s061301/package1/A.java
@@ -1,5 +1,5 @@
 package chap06.book.s061301.package1;
 
-public class A {
+class A { // package private
 
 }
--- src/chap06/book/s061301/package2/C.java
+++ src/chap06/book/s061301/package2/C.java
@@ -1,5 +1,8 @@
 package chap06.book.s061301.package2;
 
-public class C {
+import chap06.book.s061301.package1.B;
 
+public class C {
+	A a; // A클래스는 package private이어서 다른 패키지에 있는 C클래스에서 사용 불가
+	B b; // B클래스는 public이어서 다른 패키지에 있는 C클래스에서 사용 가능
 }
