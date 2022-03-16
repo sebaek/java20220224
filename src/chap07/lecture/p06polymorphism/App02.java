382f38d819cfc302b9a0841599f26e5c57f30106
sebaek <sebaek@gmail.com>
2022-03-16 10:39:27 +0900

    polymorphism

--- src/chap07/lecture/p06polymorphism/App02.java
+++ src/chap07/lecture/p06polymorphism/App02.java
@@ -1,6 +1,7 @@
 package chap07.lecture.p06polymorphism;
 
 import chap07.lecture.p99polymorphism_class.Animal;
+import chap07.lecture.p99polymorphism_class.Canine;
 import chap07.lecture.p99polymorphism_class.Chihuahua;
 import chap07.lecture.p99polymorphism_class.StreetCat;
 
@@ -12,7 +13,19 @@ public class App02 {
 		animal1.breathe();
 		animal2.breathe();
 		
-		animal1.cry();
-		animal2.cry();
+		animal1.cry(); // 실제 인스턴스의 메소드가 실행 ***********
+		animal2.cry(); // 실제 인스턴스의 메소드가 실행 ***********
+		
+		Animal animal3 = new Canine();
+		animal3.cry(); // 실제 인스턴스의 메소드가 실행
+		
 	}
 }
+
+
+
+
+
+
+
+
