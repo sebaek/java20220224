e8cfe4e50ade54b65fe463b15ca55503fe765dfa
sebaek <sebaek@gmail.com>
2022-03-17 10:11:54 +0900

    interface

---
+++ src/chap08/lecture/p01interface/MyClass3.java
@@ -0,0 +1,27 @@
+package chap08.lecture.p01interface;
+
+// 구현클래스(구상클래스, 구체클래스)
+// concrete class
+public class MyClass3 implements MyInterface3 {
+
+	@Override
+	public void method1() {
+		System.out.println("코드 작성....");
+	}
+
+	@Override
+	public void method2() {
+		
+	}
+
+	@Override
+	public void method3() {
+		
+	}
+
+	@Override
+	public void method4() {
+		
+	}
+	
+}
