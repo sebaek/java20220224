d499a4bda8398a08971278871fd2e8bebf8922d6
sebaek <sebaek@gmail.com>
2022-03-17 14:26:33 +0900

    source

--- src/chap08/lecture/p02polymorphism/App09.java
+++ src/chap08/lecture/p02polymorphism/App09.java
@@ -1,6 +1,11 @@
 package chap08.lecture.p02polymorphism;
 
-import chap08.lecture.p01interface.example_class.*;
+import java.util.Scanner;
+
+import chap08.lecture.p01interface.example_class.Cat;
+import chap08.lecture.p01interface.example_class.Chihuahua;
+import chap08.lecture.p01interface.example_class.Pet;
+import chap08.lecture.p01interface.example_class.Retriever;
 
 public class App09 {
 	public static void main(String[] args) {
@@ -11,5 +16,17 @@ public class App09 {
 		pet1.sit();
 		pet2.sit();
 		pet3.sit();
+		
+		// api source 보기
+		String str = "java";
+		Scanner scanner;
 	}
 }
+
+
+
+
+
+
+
+
