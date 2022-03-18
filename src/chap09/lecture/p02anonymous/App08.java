a8c1d523e89783ef5bbf90e95fc664fa40a828d8
sebaek <sebaek@gmail.com>
2022-03-18 12:17:29 +0900

    param

--- src/chap09/lecture/p02anonymous/App08.java
+++ src/chap09/lecture/p02anonymous/App08.java
@@ -2,6 +2,14 @@ package chap09.lecture.p02anonymous;
 
 public class App08 {
 	public static void main(String[] args) {
+		MyInterface1 o2 = new MyInterface1() {
+			@Override
+			public void method1() {
+				System.out.println("마이인터페이스1 메소드1 재정의");
+			}
+		};
+		
+		otherMethod(o2);
 		
 //		o1.method1();
 		otherMethod(new MyInterface1() {
