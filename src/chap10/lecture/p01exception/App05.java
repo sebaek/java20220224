b8f4020e3ff54b85ad356fdd7a2855be38eba35a
sebaek <sebaek@gmail.com>
2022-03-21 10:48:41 +0900

    printstractrace

--- src/chap10/lecture/p01exception/App05.java
+++ src/chap10/lecture/p01exception/App05.java
@@ -7,9 +7,9 @@ public class App05 {
 		int i = 5;
 		int j = 0;
 		
-		if (j != 0) {
+//		if (j != 0) {
 			System.out.println(i / j); // ArithmeticException
-		}
+//		}
 		
 		System.out.println("프로그램 실행 계속...");
 	}
