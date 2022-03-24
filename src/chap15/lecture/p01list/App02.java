0c994cc623068668e739e31f4a7de22eb7fe88a1
sebaek <sebaek@gmail.com>
2022-03-24 16:03:02 +0900

    max min

---
+++ src/chap15/lecture/p01list/App02.java
@@ -0,0 +1,18 @@
+package chap15.lecture.p01list;
+
+import java.util.ArrayList;
+
+public class App02 {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(5);
+		list.add(3);
+		list.add(8);
+		list.add(2);
+		
+		// 최대값, 최소값
+		
+		
+	}
+}
