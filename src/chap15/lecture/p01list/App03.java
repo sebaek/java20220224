68ed08f3e48ea428552cee5e62ebdb1d5094a792
sebaek <sebaek@gmail.com>
2022-03-24 16:32:03 +0900

    sum, avg

--- src/chap15/lecture/p01list/App03.java
+++ src/chap15/lecture/p01list/App03.java
@@ -6,18 +6,36 @@ import java.util.List;
 public class App03 {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
-		list.get(95);
-		list.get(86);
-		list.get(83);
-		list.get(92);
-		list.get(96);
-		list.get(78);
-		list.get(83);
-		list.get(93);
-		list.get(87);
-		list.get(88);
+		list.add(95);
+		list.add(86);
+		list.add(83);
+		list.add(92);
+		list.add(96);
+		list.add(78);
+		list.add(83);
+		list.add(93);
+		list.add(87);
+		list.add(88);
+		
+		int sum = 0;
+		
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		System.out.println(sum);
 		
 		
+		sum = 0;
+		for (int item : list) {
+			sum += item;
+		}
+		System.out.println(sum);
 		
+		
+		System.out.println(sum / (double) list.size());
 	}
 }
+
+
+
+
