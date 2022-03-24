a4a86cac1fd67614d072e34efc7ec3d6063a9ee3
sebaek <sebaek@gmail.com>
2022-03-24 16:21:59 +0900

    app03

---
+++ src/chap15/lecture/p01list/App03.java
@@ -0,0 +1,23 @@
+package chap15.lecture.p01list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App03 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		list.get(95);
+		list.get(86);
+		list.get(83);
+		list.get(92);
+		list.get(96);
+		list.get(78);
+		list.get(83);
+		list.get(93);
+		list.get(87);
+		list.get(88);
+		
+		
+		
+	}
+}
