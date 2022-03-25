f648b5cba40546a354bc1e7770ce22ca976b854a
sebaek <sebaek@gmail.com>
2022-03-25 12:31:54 +0900

    replaceAll

--- src/chap14/lecture/p01lambda/App09.java
+++ src/chap14/lecture/p01lambda/App09.java
@@ -1,6 +1,7 @@
 package chap14.lecture.p01lambda;
 
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class App09 {
 	public static void main(String[] args) {
@@ -17,6 +18,12 @@ class MyArrayList {
 	public void forEach(Consumer<String> action) {
 		for (String v : values) {
 			action.accept(v);;
+		}
+	}
+	
+	public void replaceAll(UnaryOperator<String> op) {
+		for (int i = 0; i < values.length; i++) {
+			values[i] = op.apply(values[i]);
 		}
 	}
 }
