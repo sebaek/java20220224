b942424743f496d19622337c7ef0ea9e38f72d75
sebaek <sebaek@gmail.com>
2022-03-25 16:09:15 +0900

    noteen

--- src/chap99/codingbat/Functional2.java
+++ src/chap99/codingbat/Functional2.java
@@ -3,6 +3,12 @@ package chap99.codingbat;
 import java.util.List;
 
 public class Functional2 {
+	public List<Integer> noTeen(List<Integer> nums) {
+		nums.removeIf(e -> e >= 13 && e <= 19);
+
+		return nums;
+	}
+
 	public List<Integer> no9(List<Integer> nums) {
 		nums.removeIf((e) -> e % 10 == 9);
 
