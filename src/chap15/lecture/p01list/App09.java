b5ffdd1f5a627d0f87fe8490147234ffdf75ff69
sebaek <sebaek@gmail.com>
2022-03-28 14:27:07 +0900

    odd

--- src/chap15/lecture/p01list/App09.java
+++ src/chap15/lecture/p01list/App09.java
@@ -3,22 +3,17 @@ package chap15.lecture.p01list;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App08 {
+public class App09 {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
+		
 		for (int i = 0; i < 10; i++) {
 			list.add(i);
 		}
 		System.out.println(list);
 		
-		// 각 element를 2배값으로 변경
-		/*
-		for (int i = 0; i < list.size(); i++) {
-			Integer origin = list.get(i);
-			list.set(i, origin * 2);
-		}*/
-		list.replaceAll(e -> e * 2);
+		// 홀수 element 삭제
 		
-		System.out.println(list);
+		System.out.println(list); // [0, 2, 4, 6, 8]
 	}
 }
