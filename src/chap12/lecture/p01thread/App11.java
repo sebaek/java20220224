3500ad5980af3add2173fb3e38cf00100621aa5f
sebaek <sebaek@gmail.com>
2022-03-28 11:12:56 +0900

    implements Runnable

---
+++ src/chap12/lecture/p01thread/App11.java
@@ -0,0 +1,41 @@
+package chap12.lecture.p01thread;
+
+import java.awt.Toolkit;
+
+public class App11 {
+
+}
+
+class MyThread111 implements Runnable {
+	@Override
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep();
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class MyThread112 implements Runnable {
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("띠링");
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
