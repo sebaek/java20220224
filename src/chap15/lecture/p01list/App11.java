7eea2c7b6e7e9d64eb8161632d00ffa821b9886c
sebaek <sebaek@gmail.com>
2022-03-29 14:14:01 +0900

    list<list>

---
+++ src/chap15/lecture/p01list/App11.java
@@ -0,0 +1,18 @@
+package chap15.lecture.p01list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App11 {
+	public static void main(String[] args) {
+		List<List<Integer>> list = new ArrayList<>();
+		list.add(Arrays.asList(95, 86));
+		list.add(Arrays.asList(83, 92, 96));
+		list.add(Arrays.asList(78, 83, 93, 87, 88));
+		
+		// 합계(881), 평균(88.1)
+		
+		
+	}
+}
