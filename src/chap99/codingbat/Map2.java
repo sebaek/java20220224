d9a2c0c6ccd2c9a8c935767497e79efd0a3de0ad
sebaek <sebaek@gmail.com>
2022-03-30 15:04:53 +0900

    wordAppend

--- src/chap99/codingbat/Map2.java
+++ src/chap99/codingbat/Map2.java
@@ -4,6 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Map2 {
+	public String wordAppend(String[] strings) {
+		String result = "";
+		Map<String, Integer> map = new HashMap<>();
+
+		for (String str : strings) {
+			String key = str;
+
+			if (map.containsKey(key)) {
+				Integer prev = map.get(key);
+				map.put(key, prev + 1);
+			} else {
+				map.put(key, 1);
+			}
+
+			Integer cur = map.get(key);
+
+			if (cur % 2 == 0) {
+				result += key;
+			}
+		}
+
+		return result;
+
+	}
+
 	public Map<String, String> firstChar(String[] strings) {
 		Map<String, String> map = new HashMap<>();
 
