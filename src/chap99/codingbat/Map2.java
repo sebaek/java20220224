9a3081bffb3b921ccc8a63db806427894fcb5748
sebaek <sebaek@gmail.com>
2022-03-30 14:33:20 +0900

    firstChar

--- src/chap99/codingbat/Map2.java
+++ src/chap99/codingbat/Map2.java
@@ -4,6 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Map2 {
+	public Map<String, String> firstChar(String[] strings) {
+		Map<String, String> map = new HashMap<>();
+
+		for (String str : strings) {
+			String key = str.substring(0, 1);
+
+			if (map.containsKey(key)) {
+				String prev = map.get(key);
+				map.put(key, prev + str);
+			} else {
+				map.put(key, str);
+			}
+		}
+
+		return map;
+	}
+
 	public Map<String, Integer> wordCount(String[] strings) {
 		Map<String, Integer> map = new HashMap<>();
 
