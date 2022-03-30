c367bd1e98d7c608ca591ead30de6b3047f3f871
sebaek <sebaek@gmail.com>
2022-03-30 10:14:07 +0900

    mapshare

--- src/chap99/codingbat/Map1.java
+++ src/chap99/codingbat/Map1.java
@@ -3,6 +3,16 @@ package chap99.codingbat;
 import java.util.Map;
 
 public class Map1 {
+	public Map<String, String> mapShare(Map<String, String> map) {
+		if (map.containsKey("a")) {
+			map.put("b", map.get("a"));
+		}
+
+		map.remove("c");
+
+		return map;
+	}
+
 	public Map<String, String> mapBully(Map<String, String> map) {
 
 		if (map.containsKey("a")) {
