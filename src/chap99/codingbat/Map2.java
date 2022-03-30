fd67be8e29b98329dc120de3f7cfd23e6f8926ad
sebaek <sebaek@gmail.com>
2022-03-30 12:21:58 +0900

    wordLen

--- src/chap99/codingbat/Map2.java
+++ src/chap99/codingbat/Map2.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Map2 {
+	public Map<String, Integer> wordLen(String[] strings) {
+		Map<String, Integer> map = new HashMap<>();
+
+		for (String str : strings) {
+			map.put(str, str.length());
+		}
+
+		return map;
+	}
+
 	public Map<String, Integer> word0(String[] strings) {
 		Map<String, Integer> map = new HashMap<>();
 
