52b89c0571d335f8399a0a6efa95aa61579f9f0a
sebaek <sebaek@gmail.com>
2022-03-30 11:08:27 +0900

    topping3

--- src/chap99/codingbat/Map1.java
+++ src/chap99/codingbat/Map1.java
@@ -3,6 +3,18 @@ package chap99.codingbat;
 import java.util.Map;
 
 public class Map1 {
+	public Map<String, String> topping3(Map<String, String> map) {
+		if (map.containsKey("potato")) {
+			map.put("fries", map.get("potato"));
+		}
+
+		if (map.containsKey("salad")) {
+			map.put("spinach", map.get("salad"));
+		}
+
+		return map;
+	}
+
 	public Map<String, String> topping2(Map<String, String> map) {
 		if (map.containsKey("ice cream")) {
 			map.put("yogurt", map.get("ice cream"));
