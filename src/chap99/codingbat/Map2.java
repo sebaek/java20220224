12a37eb67d0b4748d973c435e45f83d34b7fe202
sebaek <sebaek@gmail.com>
2022-03-30 12:29:41 +0900

    pairs

--- src/chap99/codingbat/Map2.java
+++ src/chap99/codingbat/Map2.java
@@ -4,6 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Map2 {
+	public Map<String, String> pairs(String[] strings) {
+		Map<String, String> map = new HashMap<>();
+
+		for (String str : strings) {
+			String key = str.substring(0, 1);
+			String value = str.substring(str.length() - 1);
+
+			map.put(key, value);
+		}
+
+		return map;
+	}
+
 	public Map<String, Integer> wordLen(String[] strings) {
 		Map<String, Integer> map = new HashMap<>();
 
