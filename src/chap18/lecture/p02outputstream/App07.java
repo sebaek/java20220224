83cf460793c566fd427f3086a518f42747f59869
sebaek <sebaek@gmail.com>
2022-04-01 12:26:32 +0900

    Update App07.java

--- src/chap18/lecture/p02outputstream/App07.java
+++ src/chap18/lecture/p02outputstream/App07.java
@@ -3,7 +3,7 @@ package chap18.lecture.p02outputstream;
 public class App07 {
 	public static void main(String[] args) {
 		// 원본 파일 : "picture/참조타입01.png"
-		// 복사 파일 : "output/참조타입02_copy.png"
+		// 복사 파일 : "output/참조타입01_copy.png"
 		
 	}
 }
