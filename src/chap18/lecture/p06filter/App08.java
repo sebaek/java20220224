628b254578e4fda3908fa77bd2edebf8eac15400
sebaek <sebaek@gmail.com>
2022-04-04 11:16:11 +0900

    bufferedwriter

---
+++ src/chap18/lecture/p06filter/App08.java
@@ -0,0 +1,18 @@
+package chap18.lecture.p06filter;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class App08 {
+	public static void main(String[] args) throws Exception {
+		String out = "output/FilterExample8.txt";
+		OutputStream os = new FileOutputStream(out);
+		// OutputStreamReader로 변환
+		// BufferedWriter로 변환
+		
+		// 천만개의 문자 파일에 쓰는 코드 작성
+		
+		
+		// 스트림 닫기
+	}
+}
