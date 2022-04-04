d3daa3e1905f2ca8760ff5ad54142f43a6408fab
sebaek <sebaek@gmail.com>
2022-04-04 10:37:35 +0900

    Create App06.java

---
+++ src/chap18/lecture/p06filter/App06.java
@@ -0,0 +1,11 @@
+package chap18.lecture.p06filter;
+
+public class App06 {
+	public static void main(String[] args) {
+		// BufferedReader, BufferedWriter 사용해서
+		// output/FilterExample51.txt 를
+		// output/FitterExample6.txt 로 복사
+		
+		
+	}
+}
