b8f740e6882ea794a30e0d65190f40114f47ba65
sebaek <sebaek@gmail.com>
2022-04-04 14:48:38 +0900

    server

--- src/chap18/lecture/p07network/App01Server.java
+++ src/chap18/lecture/p07network/App01Server.java
@@ -1,5 +1,9 @@
 package chap18.lecture.p07network;
 
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,7 +18,22 @@ public class App01Server {
 		
 		System.out.println("[서버쪽 소켓 생성됨]");
 		
+		OutputStream os = socket.getOutputStream();
+		Writer wr = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(wr);
+		
+		bw.write("클라이언트님 반갑습니다.");
+		
+		bw.close();
+		wr.close();
+		os.close();
+		socket.close();
+		serverSocket.close();
 		
 		System.out.println("프로그램 실행 종료..");
 	}
 }
+
+
+
+
